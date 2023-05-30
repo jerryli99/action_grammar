262dc8ec8cf634df7046bdc1abc425177d3b7622
Jerry Li <jli103@terpmail.umd.edu>
2023-05-29 21:38:40 -0400

    practiced some basic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 import os
+import types
 import numpy as np
 from matplotlib import pyplot as plt
 from PIL import Image
@@ -12,53 +13,20 @@ for filename in os.listdir(folder_path):
     if filename.endswith('.jpg') or filename.endswith('.png'):
         image_path = os.path.join(folder_path, filename)
         image = cv2.imread(image_path)
+        #pixel = image[220, 450] # [y, x]
+        #print(pixel)
+        (img_width, img_height, img_dim) = image.shape
+        for i in range (0, img_width, 80):
+            for j in range (0, img_height, 80):
+                cv2.imshow('', image[i:i+80, j:j+80])
+                cv2.waitKey(500)
+                
+        break
         if image is not None:
             # Process the image (e.g., display, save, etc.)
             cv2.imshow('Image', image)
-            cv2.waitKey(0)
+            cv2.waitKey(1)
         else:
             print(f'Error reading image: {filename}')
 
 
-
-"""
-img = cv2.imread("image.001.jpg")
-
-channels = cv2.split(img)
-
-cv2.imshow("e", channels[0])
-cv2.waitKey()
-cv2.imshow("ee", channels[1])
-cv2.waitKey()
-cv2.imshow("eee", channels[2])
-cv2.waitKey()
-"""
-
-"""
-x = Image.open("image.001.jpg")
-print(x.size)
-x = cv2.imread("image.001.jpg")
-print(x.shape)
-x=plt.imread("image.001.jpg")
-print(x.shape)
-plt.imshow(x)
-plt.show()
-"""
-
-"""
-capture = cv2.VideoCapture("test_video1.mp4")
-
-while True:
-    capture.set(cv2.CAP_PROP_FPS, 12)
-    ret, frame = capture.read()
-    if ret == False:
-        break
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2YUV)
-
-    cv2.imshow("frame", frame)
-    
-    key = cv2.waitKey(500)
-
-capture.release()
-cv2.destroyAllWindows()
-"""
